271a68554fdbf4ae37a05f3811b514b9d70c12fa
AlissonSeraphim <projetoseraphim@gmail.com>
2023-03-01 17:58:22 -0300

    Requisito 10 e Projeto Concluido!

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missions.map((mission) => (<MissionCard
+          key={ mission }
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />)) }
       </div>
     );
   }
 }
-
-// PlanetCard.propTypes = {
-//   planetName: PropTypes.string.isRequired,
-//   planetImage: PropTypes.string.isRequired,
-// };
 
 export default Missions;
