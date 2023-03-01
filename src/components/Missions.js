5e8393819268991317363bc3951edeeb77a610e1
AlissonSeraphim <projetoseraphim@gmail.com>
2023-03-01 17:42:44 -0300

    Requisito 7 OK

---
+++ src/components/Missions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+
+// PlanetCard.propTypes = {
+//   planetName: PropTypes.string.isRequired,
+//   planetImage: PropTypes.string.isRequired,
+// };
+
+export default Missions;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,9 +5,6 @@ import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
-    // const planetName = planets.map((planet) => {
-    //   return planet
-    // })
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
